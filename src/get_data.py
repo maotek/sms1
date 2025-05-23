5c8631c0c33559700575bf635cabb131670c5115
Maosheng Jiang <m.jiang-5@student.tudelft.nl>
2025-05-23 15:06:05 +0200

    changing

--- src/get_data.py
+++ src/get_data.py
@@ -4,8 +4,8 @@ Download and extract data.
 import urllib.request
 import zipfile
 
-# URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
-URL = 'https://surfdrive.surf.nl/files/index.php/s/OZRd9BcxhGkxTuy/download' # v2, 2000 datapoints
+URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
+# URL = 'https://surfdrive.surf.nl/files/index.php/s/OZRd9BcxhGkxTuy/download' # v2, 2000 datapoints
 
 EXTRACT_DIR = "smsspamcollection"
 
