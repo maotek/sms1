8fbcbc78c3b5d900492378a53fb14d742372bad2
Maosheng Jiang <m.jiang-5@student.tudelft.nl>
2025-05-23 15:19:43 +0200

    google drive

--- src/get_data.py
+++ src/get_data.py
@@ -4,8 +4,11 @@ Download and extract data.
 import urllib.request
 import zipfile
 
-URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
+# URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
 # URL = 'https://surfdrive.surf.nl/files/index.php/s/OZRd9BcxhGkxTuy/download' # v2, 2000 datapoints
+
+
+URL = 'https://surfdrive.surf.nl/files/index.php/s/HU5mY29RzxRlHCU/download' # v4 3000 datapoints
 
 EXTRACT_DIR = "smsspamcollection"
 
